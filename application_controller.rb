3022fbd5c81592a6b0c57b496241940c891f9b94
TheMasterGabriel <egabriel@riverdale.edu>
2015-08-27 21:53:19 +0000

    Eigth Commit

--- application_controller.rb
+++ application_controller.rb
@@ -1,4 +1,5 @@
 require 'bundler'
+require_relative 'models/Dragon.rb'
 Bundler.require
 
 class MyApp < Sinatra::Base
@@ -7,4 +8,17 @@ class MyApp < Sinatra::Base
     erb :index
   end
 
+  get '/create_character' do
+    erb :index
+  end
+
+  get '/create_character/random' do
+    @random_dragon = Dragon.random
+    erb :create_character
+  end
+
+  post '/create_character' do
+    @random_dragon = nil
+    erb :create_character
+  end
 end
