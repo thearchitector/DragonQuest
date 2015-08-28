b1390c3d2f2fa8207389da3287efccee0e54c8b7
Darkspine77 <Jwoods@Riverdale.edu>
2015-08-28 17:03:32 +0000

    Thirteenth Commit

--- application_controller.rb
+++ application_controller.rb
@@ -27,145 +27,135 @@ class MyApp < Sinatra::Base
     erb :preview_character
   end
 
-  get '/copy_me' do
-    @user_dragon = Dragon.new("Chuck Norris", "Male", "YellowGreen", "Leathery")
-    erb :copy_me
-  end
-
   post '/story/page1' do
+    puts params
     @user_dragon = Dragon.new(params["name"], params["sex"], params["color"], params["skin_type"])
-    @user_dragon.health=(params["health"])
-    @user_dragon.gold=(params["gold"])
     erb :'/story/page1'
   end
 
   post '/story/page2' do
     @user_dragon = Dragon.new(params["name"], params["sex"], params["color"], params["skin_type"])
-    @user_dragon.health=(params["health"])
-    @user_dragon.gold=(params["gold"])
+    @user_dragon.health=(params["health"].to_i)
+    @user_dragon.gold=(params["gold"].to_i + 100)
     erb :'/story/page2'
-  end
-
-  get '/copy_me' do
-    erb :copy_me
   end
 
   post '/story/page3' do
     @user_dragon = Dragon.new(params["name"], params["sex"], params["color"], params["skin_type"])
-    @user_dragon.health=(params["health"])
-    @user_dragon.gold=(params["gold"])
+    @user_dragon.health=(params["health"].to_i)
+    @user_dragon.gold=(params["gold"].to_i)
     erb :'/story/page3'
   end
 
   post '/story/page4' do
     @user_dragon = Dragon.new(params["name"], params["sex"], params["color"], params["skin_type"])
-    @user_dragon.health=(params["health"])
-    @user_dragon.gold=(params["gold"])
+    @user_dragon.health=(params["health"].to_i)
+    @user_dragon.gold=(params["gold"].to_i)
     erb :'/story/page4'
   end
 
   post '/story/page5' do
     @user_dragon = Dragon.new(params["name"], params["sex"], params["color"], params["skin_type"])
-    @user_dragon.health=(params["health"])
-    @user_dragon.gold=(params["gold"])
+    @user_dragon.health=(params["health"].to_i - 3)
+    @user_dragon.gold=(params["gold"].to_i)
     erb :'/story/page5'
   end
 
   post '/story/page6' do
     @user_dragon = Dragon.new(params["name"], params["sex"], params["color"], params["skin_type"])
-    @user_dragon.health=(params["health"])
-    @user_dragon.gold=(params["gold"])
+    @user_dragon.health=(params["health"].to_i)
+    @user_dragon.gold=(params["gold"].to_i + 100)
     erb :'/story/page6'
   end
 
   post '/story/page7' do
     @user_dragon = Dragon.new(params["name"], params["sex"], params["color"], params["skin_type"])
-    @user_dragon.health=(params["health"])
-    @user_dragon.gold=(params["gold"])
+    @user_dragon.health=(params["health"].to_i)
+    @user_dragon.gold=(params["gold"].to_i + 200)
     erb :'/story/page7'
   end
 
   post '/story/page8' do
     @user_dragon = Dragon.new(params["name"], params["sex"], params["color"], params["skin_type"])
-    @user_dragon.health=(params["health"])
-    @user_dragon.gold=(params["gold"])
+    @user_dragon.health=(params["health"].to_i)
+    @user_dragon.gold=(params["gold"].to_i)
     erb :'/story/page8'
   end
 
   post '/story/page9' do
     @user_dragon = Dragon.new(params["name"], params["sex"], params["color"], params["skin_type"])
-    @user_dragon.health=(params["health"])
-    @user_dragon.gold=(params["gold"])
+    @user_dragon.health=(params["health"].to_i)
+    @user_dragon.gold=(params["gold"].to_i)
     erb :'/story/page9'
   end
 
   post '/story/page10' do
     @user_dragon = Dragon.new(params["name"], params["sex"], params["color"], params["skin_type"])
-    @user_dragon.health=(params["health"])
-    @user_dragon.gold=(params["gold"])
+    @user_dragon.health=(params["health"].to_i)
+    @user_dragon.gold=(params["gold"].to_i)
     erb :'/story/page10'
   end
 
   post '/story/page11' do
     @user_dragon = Dragon.new(params["name"], params["sex"], params["color"], params["skin_type"])
-    @user_dragon.health=(params["health"])
-    @user_dragon.gold=(params["gold"])
+    @user_dragon.health=(params["health"].to_i)
+    @user_dragon.gold=(params["gold"].to_i)
     erb :'/story/page11'
   end
 
   post '/story/page12' do
     @user_dragon = Dragon.new(params["name"], params["sex"], params["color"], params["skin_type"])
-    @user_dragon.health=(params["health"])
-    @user_dragon.gold=(params["gold"])
+    @user_dragon.health=(params["health"].to_i)
+    @user_dragon.gold=(params["gold"].to_i)
     erb :'/story/page12'
   end
 
   post '/story/page13' do
     @user_dragon = Dragon.new(params["name"], params["sex"], params["color"], params["skin_type"])
-    @user_dragon.health=(params["health"])
-    @user_dragon.gold=(params["gold"])
+    @user_dragon.health=(params["health"].to_i)
+    @user_dragon.gold=(params["gold"].to_i)
     erb :'/story/page13'
   end
 
   post '/story/page14' do
     @user_dragon = Dragon.new(params["name"], params["sex"], params["color"], params["skin_type"])
-    @user_dragon.health=(params["health"])
-    @user_dragon.gold=(params["gold"])
+    @user_dragon.health=(params["health"].to_i)
+    @user_dragon.gold=(params["gold"].to_i + 400)
     erb :'/story/page14'
   end
 
   post '/story/page15' do
     @user_dragon = Dragon.new(params["name"], params["sex"], params["color"], params["skin_type"])
-    @user_dragon.health=(params["health"])
-    @user_dragon.gold=(params["gold"])
+    @user_dragon.health=(params["health"].to_i)
+    @user_dragon.gold=(params["gold"].to_i)
     erb :'/story/page15'
   end
 
   post '/story/page16' do
     @user_dragon = Dragon.new(params["name"], params["sex"], params["color"], params["skin_type"])
-    @user_dragon.health=(params["health"])
-    @user_dragon.gold=(params["gold"])
+    @user_dragon.health=(params["health"].to_i)
+    @user_dragon.gold=(params["gold"].to_i)
     erb :'/story/page16'
   end
 
   post '/story/page17' do
     @user_dragon = Dragon.new(params["name"], params["sex"], params["color"], params["skin_type"])
-    @user_dragon.health=(params["health"])
-    @user_dragon.gold=(params["gold"])
+    @user_dragon.health=(params["health"].to_i)
+    @user_dragon.gold=(params["gold"].to_i + 600)
     erb :'/story/page17'
   end
 
   post '/story/page18' do
     @user_dragon = Dragon.new(params["name"], params["sex"], params["color"], params["skin_type"])
-    @user_dragon.health=(params["health"])
-    @user_dragon.gold=(params["gold"])
+    @user_dragon.health=(params["health"].to_i)
+    @user_dragon.gold=(params["gold"].to_i + 500)
     erb :'/story/page18'
   end
 
   post '/story/page19' do
     @user_dragon = Dragon.new(params["name"], params["sex"], params["color"], params["skin_type"])
-    @user_dragon.health=(params["health"])
-    @user_dragon.gold=(params["gold"])
+    @user_dragon.health=(params["health"].to_i)
+    @user_dragon.gold=(params["gold"].to_i)
     erb :'/story/page19'
   end
 
